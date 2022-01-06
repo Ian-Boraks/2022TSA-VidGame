82f04f0a029a41d882ec21278269131bc80cb4cf
IanBoraks <ianboraks@gmail.com>
2022-01-06 10:03:50 -0700

    Orginizing

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 // ! Feel free to remove any of these and add your own functions
 
+// * ON LOAD --------------------------------------------------------
 window.addEventListener('DOMContentLoaded', function () {
   // ! This is an example of how to use the sound function
   // ! Now when you click the screen the funny sound will play
@@ -9,6 +10,19 @@ window.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+soundManager.onready(function () {
+  // ! All sound effects need to be loaded with this function before they can be played
+  soundManager.createSound({
+    id: 'trombone',
+    url: '/assets/sound/FX68GWY-funny-trombone-slide-accent.mp3'
+  });
+
+  // playSound('trombone');
+});
+
+// * CLASSES ----------------------------------------------------------
+
+// * FUNCTIONS --------------------------------------------------------
 function playerMovement(params) {
   
 }
@@ -25,16 +39,7 @@ function enemyCollision(params) {
 
 }
 
-// * Modified from: https://stackoverflow.com/questions/9419263/how-to-play-audio
+// ? Modified from: https://stackoverflow.com/questions/9419263/how-to-play-audio
 function playSound(sound) {
   soundManager.play(sound);
 }
-
-soundManager.onready(function () {
-  soundManager.createSound({
-    id: 'trombone',
-    url: '/assets/sound/FX68GWY-funny-trombone-slide-accent.mp3'
-  });
-
-  // playSound('trombone');
-});
