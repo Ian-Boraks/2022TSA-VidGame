c2000f26fe8a34a9f79d8aa30635a0f608aa8a65
IanBoraks <ianboraks@gmail.com>
2022-01-06 09:25:33 -0700

    Some set up functions

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+// ! Feel free to remove any of these and add your own functions
+
+function playerMovement(params) {
+  
+}
+
+function playerCollision(params) {
+
+}
+
+function enemyMovement(params) {
+
+}
+
+function enemyCollision(params) {
+
+}
