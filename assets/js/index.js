3f0d0835b381497424a1d88f081bbe2db9aa8a96
IanBoraks <ianboraks@gmail.com>
2022-01-06 09:39:12 -0700

    Sound Example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,14 @@
 // ! Feel free to remove any of these and add your own functions
 
+window.addEventListener('DOMContentLoaded', function () {
+  // ! This is an example of how to use the sound function
+  // ! Now when you click the screen the funny sound will play
+  var gameWindow = document.getElementById('game-window');
+  gameWindow.addEventListener('click', function () { 
+    playSound('trombone');
+  });
+});
+
 function playerMovement(params) {
   
 }
@@ -15,3 +24,17 @@ function enemyMovement(params) {
 function enemyCollision(params) {
 
 }
+
+// * Modified from: https://stackoverflow.com/questions/9419263/how-to-play-audio
+function playSound(sound) {
+  soundManager.play(sound);
+}
+
+soundManager.onready(function () {
+  soundManager.createSound({
+    id: 'trombone',
+    url: '/assets/sound/FX68GWY-funny-trombone-slide-accent.mp3'
+  });
+
+  // playSound('trombone');
+});
